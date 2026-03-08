5f4a40cb5a1df3d3eeeb52f4193a9a095d9dbbf9
Alexander Sumer <alexandersumer7@gmail.com>
2026-03-08 17:06:12 +1100

    Remove unnecessary bearblog redirect from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ export default defineConfig({
   site: 'https://alexandersumer.com',
   output: 'static',
   integrations: [sitemap()],
-  redirects: {
-    '/what-to-do-if-you-take-agi-seriously': '/blog/what-to-do-if-you-take-agi-seriously/',
-  },
   markdown: {
     shikiConfig: {
       themes: {
